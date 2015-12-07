4ec49ac9f3c119bc93383832da91a74d4dc33e73
Juan Pablo Prado <juan@juanpabloprado.com>
2015-12-07 17:25:33 -0500

    Add log calls cardEventListener

--- app/src/main/java/com/juanpabloprado/cardstacksample/MainActivity.java
+++ app/src/main/java/com/juanpabloprado/cardstacksample/MainActivity.java
@@ -3,13 +3,80 @@ package com.juanpabloprado.cardstacksample;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 import com.wenchao.cardstack.CardStack;
 
 public class MainActivity extends AppCompatActivity {
-  private CardStack mCardStack;
-  private CardsDataAdapter mCardAdapter;
+  private static final String TAG = "MainActivity";
+  private static final int DIRECTION_ZERO = 0;
+  private static final int DIRECTION_ONE = 1;
+  private static final int DIRECTION_TWO = 2;
+  private static final int DIRECTION_THREE = 3;
+
+  private CardStack.CardEventListener cardEventListener = new CardStack.CardEventListener() {
+    // implement card event interface
+    @Override public boolean swipeEnd(int direction, float distance) {
+      // if "return true" the dismiss animation will be triggered
+      // if false, the card will move back to stack
+      // distance is finger swipe distance in dp
+
+      // the direction indicate swipe direction
+      // there are four directions
+      //  0  |  1
+      // ----------
+      //  2  |  3
+
+      Log.i(TAG,
+          "CardStack swipeEnd triggered with direction: " + direction + ", distance: " + distance);
+      return distance > 300;
+    }
+
+    @Override public boolean swipeStart(int direction, float distance) {
+
+      Log.i(TAG, "CardStack swipeStart triggered with direction: "
+          + direction
+          + ", distance: "
+          + distance);
+      return true;
+    }
+
+    @Override public boolean swipeContinue(int direction, float distanceX, float distanceY) {
+      Log.i(TAG, "CardStack swipeContinue triggered with direction: "
+          + direction
+          + ", distanceX: "
+          + distanceX
+          + ", distanceY: "
+          + distanceY);
+      return true;
+    }
+
+    @Override public void discarded(int id, int direction) {
+      //this callback invoked when dismiss animation is finished.
+      Log.i(TAG, "CardStack discarded triggered with id: " + id + ", direction: " + direction);
+      switch (direction) {
+        case DIRECTION_ZERO:
+          Toast.makeText(MainActivity.this, "Direction: " + direction, Toast.LENGTH_SHORT).show();
+          break;
+        case DIRECTION_ONE:
+          Toast.makeText(MainActivity.this, "Direction: " + direction, Toast.LENGTH_SHORT).show();
+          break;
+        case DIRECTION_TWO:
+          Toast.makeText(MainActivity.this, "Direction: " + direction, Toast.LENGTH_SHORT).show();
+          break;
+        case DIRECTION_THREE:
+          Toast.makeText(MainActivity.this, "Direction: " + direction, Toast.LENGTH_SHORT).show();
+          break;
+      }
+    }
+
+    @Override public void topCardTapped() {
+      //this callback invoked when a top card is tapped by user.
+      Log.i(TAG, "CardStack topCardTapped triggered");
+    }
+  };
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -17,21 +84,18 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
-    mCardStack = (CardStack)findViewById(R.id.cardStackContainer);
+    CardStack mCardStack = (CardStack) findViewById(R.id.cardStackContainer);
 
-    //
     mCardStack.setContentResource(R.layout.card_content);
     mCardStack.setStackMargin(20);
+    mCardStack.setListener(cardEventListener);
 
-    //
-    mCardAdapter = new CardsDataAdapter(getApplicationContext(),0);
+    CardsDataAdapter mCardAdapter = new CardsDataAdapter(getApplicationContext(), 0);
     mCardAdapter.add("test1");
     mCardAdapter.add("test2");
     mCardAdapter.add("test3");
     mCardAdapter.add("test4");
     mCardAdapter.add("test5");
-
-
 
     mCardStack.setAdapter(mCardAdapter);
   }
@@ -48,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
     // as you specify a parent activity in AndroidManifest.xml.
     int id = item.getItemId();
 
-    //noinspection SimplifiableIfStatement
+    // noinspection SimplifiableIfStatement
     if (id == R.id.action_settings) {
       return true;
     }
